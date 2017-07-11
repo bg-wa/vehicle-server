df6057fdaaeaa5e81e76c78ea29919e925b76c4c
bg-wa <bill.j.grow@gmail.com>
2017-07-10 20:56:48 -0700

    remote

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
 
   get 'dashboard/index'
-  get 'remote/index'
+  get 'remote', to: 'remote#index'
 
   scope 'api' do
     jsonapi_resource :dht11_readings
